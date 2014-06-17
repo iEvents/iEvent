0a16950270db39d42ae9808bc73a4dd4ba93aefa
pust3kuch3n <mai.ngoc.duy@hotmail.ch>
2014-06-17 14:42:26 +0200

    updated model.js

--- js/model.js
+++ js/model.js
@@ -1,8 +1,8 @@
-/** Objekt Wertung repräsentiert eine einzelne 
- * Bewertung eines Whisky.
+/** Objekt Event repräsentiert eine einzelnes 
+ * erstelltes Event.
  */
-var Wertung = Class.extend({
-   init : function(date, distillery, bezeichnung, fass, proof, finishing, nr, wertung, gtorf, gsherry, gholz, gfrucht, gflora, gfeinty, finish, kommentar, ort, guid) {
+var Event = Class.extend({
+   init : function(eventName, description, participants, location, map, date, time, guid) {
       // GUID als ID (somit keine Probleme bei Verteilten Einträgen)
       if(guid == null)
          this.guid = this.generateGUID();
@@ -10,54 +10,42 @@ var Wertung = Class.extend({
          this.guid = guid;
 
       // Allgemeine Parameter
+      this.eventName = eventName;
+      this.description = description;
+      this.paraticipants = participants;
+      this.location = location;
       this.date = date;
-      this.distillery = distillery;
-      this.bezeichnung = bezeichnung;
-      this.fass = fass;
-      this.proof = proof; // Fassstärke?
-      this.finishing = finishing;
-      this.nr = nr; // Proben oder Fassnummer
-      
-      // Bewertung Whisky
-      this.wertung = wertung;
-      this.gtorf = gtorf;
-      this.gsherry = gsherry;
-      this.gholz = gholz;
-      this.gfrucht = gfrucht;
-      this.gflora = gflora;
-      this.gfeinty = gfeinty;
-      this.finish = finish;
-      this.kommentar = kommentar;
+      this.time = time;
    
-      // Ort der Bewertung
-      if(ort == null)
-         this.ort = "";
+      // Ort durch GeoLocation
+      if(map == null)
+         this.map = "";
       else
-         this.ort = ort;
+         this.map = map;
    },
    
-   /** Gibt eine Bezeichnung der Wertung zurück.
+   /** Gibt eine Bezeichnung des Events zurück.
     */
    getName : function() {
-      return this.distillery + ": " +  this.bezeichnung + " - Wertung am " + this.date;
+      return this.eventName + ": " +  this.description + " - am " + this.date;
    }
 });
 
-/** Objekt Tastings - Container für Wertungen
+/** Objekt Happenings - Container für Events
  *  mit den üblichen CRUD-Funktionen.
  */
-var Tastings = Observer.extend({
+var Happenings = Observer.extend({
    init : function() {
       this._super();
-      this.wertungsAr = new Array();
+      this.eventAr = new Array();
    },
    
    // Nun die CRUD Funktionen
-   /** Wertungen setzen und DB aktualisieren */
-   setWertungenWithDB : function(array) {
-      // Alle Wertungen löschen
-      for ( var i = 0; i < this.wertungsAr.length; i++) {
-         var deleted = this.wertungsAr.splice(i, 1);
+   /** Event setzen und DB aktualisieren */
+   setEventWithDB : function(array) {
+      // Alle Events löschen
+      for ( var i = 0; i < this.eventAr.length; i++) {
+         var deleted = this.eventAr.splice(i, 1);
          //this.notify({obj: deleted[0], crud: "D"});
       }
       WhiskyApp.db.delAll();
@@ -66,88 +54,86 @@ var Tastings = Observer.extend({
       dt.setTime(dt.getTime() + 1000);
       while (new Date().getTime() < dt.getTime());
       
-      // Wertungen hinzufügen
+      // Event hinzufügen
       for ( var i = 0; i < array.length; i++) {
          this.add(array[i]);
       }
    },
    
-   /** Wertungen setzen */
-   setWertungen : function(array) {
-      this.wertungsAr = array;
+   /** Events setzen */
+   setEvents : function(array) {
+      this.eventAr = array;
 
       // update
-      this.notify({obj: this.wertungsAr, crud: "R"});
+      this.notify({obj: this.eventAr, crud: "R"});
    },
    
-   /** (C) Erzeugt eine Wertung und fügt diese hinzu.
+   /** (C) Erzeugt ein Event und fügt diese hinzu.
     */
-   create : function(date, distillery, bezeichnung, fass, proof, finishing, nr, wertung, gtorf, gsherry, gholz, gfrucht,
-         gflora, gfeinty, finish, kommentar, ort) {
+   create : function(eventName, description, participants, location, map, date, time) {
    
-      // Wertung erzeugen
-   	var wertung = new Wertung(date, distillery, bezeichnung, fass, proof, finishing, nr, wertung, gtorf, gsherry, gholz, gfrucht,
-         gflora, gfeinty, finish, kommentar, ort);
+      // Event erzeugen
+   	var event = new Event(eventName, description, participants, location, map, date, time);
    
       // dem Array hinzufügen      
-      this.wertungsAr.push(wertung);
+      this.eventAr.push(event);
 
       // update
-      this.notify({obj: wertung, crud: "C"});
+      this.notify({obj: event, crud: "C"});
    },
    
-   /** (C) Fügt einen Eintrag hinzu, mit Wertungs-Objekt
+   /** (C) Fügt einen Eintrag hinzu, mit Event-Objekt
     */
-   add : function(wertung) {
+   add : function(event) {
          // dem Array hinzufügen      
-      this.wertungsAr.push(wertung);
+      this.eventAr.push(event);
 
       // update
-      this.notify({obj: wertung, crud: "C"});
+      this.notify({obj: event, crud: "C"});
    },
    
-   /** (R) Sucht die Wertung mit der GUID.
+   /** (R) Sucht den Event mit der GUID.
     */
-   getWertungByID : function(guid) {
-   	for ( var i = 0; i < this.wertungsAr.length; i++) {
-   		if (this.wertungsAr[i].guid == guid) {
-   			return this.wertungsAr[i];
+   getEventByID : function(guid) {
+   	for ( var i = 0; i < this.eventAr.length; i++) {
+   		if (this.eventAr[i].guid == guid) {
+   			return this.eventAr[i];
    		}
    	}
    },
    
-   /** (R) Array mit Wertungen zurückgeben
+   /** (R) Array mit Events zurückgeben
     */
-   getWertungen : function() {
-      return this.wertungsAr;
+   getEvents : function() {
+      return this.eventAr;
    },
    
-   /** (U) Aktualisiert die Wertung.
+   /** (U) Aktualisiert den Event.
     * Wenn diese nicht vorhanden ist, wird eine neue erzeugt.
     */
-   edit : function(wertung) {
+   edit : function(event) {
       var gefunden = 0;
       
-      for ( var i = 0; i < this.wertungsAr.length; i++) {
-         if (this.wertungsAr[i].guid == wertung.guid) {
-            // Wertung gefunden, nun ersetzen
-            this.wertungsAr[i] = wertung;
+      for ( var i = 0; i < this.eventAr.length; i++) {
+         if (this.eventAr[i].guid == event.guid) {
+            // Event gefunden, nun ersetzen
+            this.eventAr[i] = event;
             gefunden = 1;
-            this.notify({obj: wertung, crud: "U"});
+            this.notify({obj: event, crud: "U"});
          }
       }
       
       // existiert keine, dann hinzufügen
       if( gefunden == 0 )
-         this.add(wertung);
+         this.add(event);
    },
 
-   /** (D) Löscht die Wertung mit der GUID.
+   /** (D) Löscht den Event mit der GUID.
     */
    deleteID : function(guid) {
-   	for ( var i = 0; i < this.wertungsAr.length; i++) {
-   		if (this.wertungsAr[i].guid == guid) {
-   			var deleted = this.wertungsAr.splice(i, 1);
+   	for ( var i = 0; i < this.eventAr.length; i++) {
+   		if (this.eventAr[i].guid == guid) {
+   			var deleted = this.eventAr.splice(i, 1);
             this.notify({obj: deleted[0], crud: "D"});
    			return;
    		}
